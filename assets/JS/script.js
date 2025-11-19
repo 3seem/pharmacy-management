a8545204ec7b8700d3f619136863c388f8f4c5d1
Reem Adel <reemadel316@gmail.com>
2025-11-19 23:20:02 +0200

    Initial commit - project setup

---
+++ assets/JS/script.js
@@ -0,0 +1,8 @@
+//Product script
+ document.querySelectorAll('.details').forEach(item => {
+    item.addEventListener("click", () => {
+      window.location.href = "../Customer/product_details.html";
+    });
+  });
+
+
