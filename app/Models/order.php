fe5f1d26342a305ba6a95d78e3944bccda47150b
Ahmedsalah-28 <aahmedasd936@gmail.com>
2025-12-20 19:31:48 +0200

    order_item

--- app/Models/order.php
+++ app/Models/order.php
@@ -8,7 +8,7 @@ class Order extends Model
 {
             public function items()
         {
-            return $this->hasMany(OrderItem::class);
+            return $this->hasMany(order_item::class);
         }
 
         public function product()
