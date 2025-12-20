b420f606c1c27f504b354d575e153f05f0193f5f
3ssem <mohammedassem246@gmail.com>
2025-12-20 22:40:28 +0200

    conflicts

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,9 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $primaryKey = 'Order_ID';
+    protected $fillable = [
+        'Order_Date',
+        'Total_amount',
+        'Payment_method',
+        'Status',
+        'Customer_ID',
+        'Employee_ID',
+        'discount_amount',
+        'tax_amount',
+        'delivery_address',
+        'delivery_type',
+        'notes',
+    ];
+    // relation with customer
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'Customer_ID', 'id');
+    }
+    // relation with employee
+    public function employee()
+    {
+        return $this->belongsTo(User::class, 'Employee_ID', 'id');
+    }
             public function items()
         {
-            return $this->hasMany(OrderItem::class);
+            return $this->hasMany(order_item::class);
         }
 
         public function product()
